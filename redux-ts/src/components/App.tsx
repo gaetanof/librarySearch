b75d26813af4a637e77a8373bff98a14407a2234
gaetanof <franciscogaetano1@gmail.com>
2023-10-02 17:56:52 -0300

    final commit, app working

--- redux-ts/src/components/App.tsx
+++ redux-ts/src/components/App.tsx
@@ -1,5 +1,16 @@
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
+
 const App = () => {
-  return <h1>Hi!</h1>;
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search For a Package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
